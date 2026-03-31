190e8e9b3bceade9f4663a387d944027912876c0
Alexandros-Digitalos <152096497+Alexandros-Digitalos@users.noreply.github.com>
2026-03-31 09:20:36 +0300

    Save before big refracture
    The code here isn't working

--- Entities/main/main.gd
+++ Entities/main/main.gd
@@ -15,35 +15,48 @@ func _ready() -> void:
 	filter_settings.protocol_filter_sent.connect(func (data: String) -> void: filter_protocol = data)
 	packet_loss_probability.percent_value_changed.connect(func (data: float) -> void: loss_percange = data)
 
-func _process(_delta: float) -> void:
-	if GLOBAL.server.is_connection_available():
-		## UDP packet peer, used to send and receive raw UDP packets and Variants.
-		## Returns the first pending connection, null if no new connection is available.
-		var peer: PacketPeerUDP = GLOBAL.server.take_connection()
-		print("Accepted peer: %s :%s" % [peer.get_packet_ip(), peer.get_packet_port()])
+#func _process(_delta: float) -> void:
+	#if GLOBAL.server.is_connection_available():
+		#print("Connection Found")
+#
+		### UDP packet peer, used to send and receive raw UDP packets and Variants.
+		### Returns the first pending connection, null if no new connection is available.
+		#var peer: PacketPeerUDP = GLOBAL.server.take_connection()
+		#print("Accepted peer: %s :%s" % [peer.get_packet_ip(), peer.get_packet_port()])
+#
+		### Recieved data from Python
+		#var packet: PackedByteArray  = peer.get_packet()
+		##print("Packet: %s" % packet)
+		#print("Received Packet Data: %s" % [packet.get_string_from_utf8()])
+#
+		### Reply so it knows we received the message.
+		#peer.put_packet("Hello from Godot!".to_utf8_buffer())
+#
+		#### Keep a reference so we can keep contacting the remote peer.
+		##peers.append(peer)
+		##print("Peers list %s" % peers)
 
-		## Recieved data from Python
-		var packet: PackedByteArray  = peer.get_packet()
-		#print("Packet: %s" % packet)
-		print("Received Packet Data: %s" % [packet.get_string_from_utf8()])
+func send_params_to_python() -> void:
+	## UDP packet peer, used to send and receive raw UDP packets and Variants.
+	## Returns the first pending connection, null if no new connection is available.
+	var peer: PacketPeerUDP = GLOBAL.server.take_connection()
+	print("Accepted peer: %s :%s" % [peer.get_packet_ip(), peer.get_packet_port()])
 
-		## Reply so it knows we received the message.
-		peer.put_packet("Hello from Godot!".to_utf8_buffer())
+	## Send the parameters of the filters and percentage to python
+	var params_msg: String = "ip=%s;port=%s;proto=%s;loss=%f" % [filter_ip, filter_port, filter_protocol, loss_percange]
+	peer.put_packet(params_msg.to_utf8_buffer())
 
-		""" ## Keep a reference so we can keep contacting the remote peer.
-		peers.append(peer)
-		print("Peers list %s" % peers)
-
-		for i: int in range(0, peers.size()):
-			print(peers[i].get_packet().get_string_from_utf8())
-		#filter_ip
-		#filter_port
-		#filter_protocol"""
-	#else:
-		#print_debug("No Connection Found")
+	## Keep a reference so we can keep contacting the remote peer.
+	peers.append(peer)
+	print("Peers list %s" % peers)
 
 func execute_py() -> void:
 	OS.execute("python", [GLOBAL.script_path], [])
+	if GLOBAL.server.is_connection_available():
+		print("Connection Found")
+		send_params_to_python()
+	else:
+		print("No Connection Found")
 
 func _on_temp_button_pressed() -> void:
 	execute_py()
--- PythonFiles/packet_capture.py
+++ PythonFiles/packet_capture.py
@@ -1,22 +1,24 @@
 from pydivert import *
-
-import os
-import logging
-import socket
-
+import os; import logging; import socket
 from threading import Thread
 from time import sleep
 
-exit_time: int = 12
-
+## Connection settings
 ip: str = "127.0.0.1"
 port: int = 4242
 
+## Modification parameters
+filter_ip: int
+filter_port: int
+filter_protocol: str
+loss_percentage: float
+
+## Program exit time, cuz it frigging crashes my Parsec
+exit_time: int = 3
+
+## Log file number, cuz log_file() gets called when a packet is captured and so it gets counted globally
 log_num: int = 1
-
 LOG_FOLDER = r"D:\Projects\Game Projects\Godot Projects\School-Project\PythonFiles\Logs"
-
-curr_packet: Packet
 
 def del_logs(): 
     for i in os.listdir(LOG_FOLDER):
@@ -28,17 +30,22 @@ def log_files(data: str):
     global log_num
     ## Set the log name and formatm then the other settings
     log_path = os.path.join(LOG_FOLDER, f"captr_pckt_{log_num}.log")
-    logging.basicConfig(filename=log_path, force=True, level=logging.INFO, filemode="w",encoding="utf-8", format="%(message)s")
-    logger = logging.getLogger(__name__)
-    
-    ## Log the message
-    logger.info(data)
+    with open(log_path, "w", encoding="utf-8") as f:
+        f.write(data)
     print("GET LOGGED B*TCH ", log_num, "\n")
     log_num += 1
 
+## TODO Do modifications to the packet here
+def modify_packet(packet: Packet):
+    return packet
 
-def talk_to_godot(packet: Packet):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def send_mod_pack_to_godot(packet: Packet):
+    pass
+
+"""
+## OLD But idk if still useful
+def connect_to_godot():
+
     try:
         msg = str(packet)
         client_socket.sendto(msg.encode("utf-8"), (ip, port))
@@ -49,51 +56,56 @@ def talk_to_godot(packet: Packet):
 
     finally:
         client_socket.close()
-    """
-    ## FIXME This line bellow has a problem. It explodes when it's called inside the capture packets
-    print("code works here")
-    data, (recv_ip, recv_port) = client_socket.recvfrom(4096)
-    print("does it work here?")
-    print(f"Received: '{data.decode()}' {recv_ip}:{recv_port}")
-    """
-
+"""
 
 def capture_packets():
-    global curr_packet
-    del_logs()
-
-    with WinDivert("ip and not loopback and not (udp.DstPort == 4242 or udp.SrcPort == 4242)") as wdiv:
+    with WinDivert("true") as wdiv:
         for packet in wdiv:
+            modified_packet: Packet
             try:
-                # print(f"From Packet_capture.py \n{packet}")
-                # Sends the packet info to Godot
-                talk_to_godot(curr_packet)
-
-                log_files(str(curr_packet))
-
-                wdiv.send(curr_packet) ## This injects the packet into the netwrok
-                # print(curr_packet)
-                # break
-                sleep(.2) 
+                if (packet.udp is not None) and (packet.dst_port == 4242 or packet.src_port == 4242):
+                    continue
+                # log_files(str(packet))
+                modified_packet = modify_packet(packet) ## Modifies the packet with the given settings
+                send_mod_pack_to_godot(modified_packet) ## Sends the modified packet to godot
+                wdiv.send(modified_packet) ## This injects the packet into the netwrok
+                sleep(.5) 
 
             except Exception as e:
                 print(f"Capture error: {e}")
 
+def run_program():
+    ## Deletes the prevous logged packets 
+    del_logs()
 
-# def filter_packet(packet):
-#     new_ip = input()
-#     new_port = input()
-#     new_protocol = input()
-#     filtered_packet = packet
-#     return filtered_packet
+    ## Connect to Godot
+    connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    connection_socket.bind((ip, port))
+    connection_socket.settimeout(0.1)
 
-# def packet_loss_percantage(packet):
-#     percantage = 0
-#     filtered_packet = packet
-#     return filtered_packet
+    ## Recieve_godot_params:
+    global filter_ip, filter_port, filter_protocol, loss_percentage
+    data, addr = connection_socket.recvfrom(1024)  
+    message = data.decode("utf-8")
+    try:
+        data, addr = connection_socket.recvfrom(1024)  # buffer size
+        message = data.decode("utf-8")  # message should be string like "ip=1.2.3.4;port=80;proto=udp;loss=10.0"
+        for part in message.split(";"):
+            key, val = part.split("=")
+            if key == "ip":
+                filter_ip = val
+            elif key == "port":
+                filter_port = val
+            elif key == "proto":
+                filter_protocol = val
+            elif key == "loss":
+                loss_percentage = float(val)
+        print(f"Received params from Godot: {filter_ip}, {filter_port}, {filter_protocol}, {loss_percentage}")
+    except socket.timeout:
+        pass  # no data received this frame
 
-t = Thread(target=capture_packets, daemon=True)
-t.start()
-sleep(exit_time)
+    t = Thread(target=capture_packets, daemon=True)
+    t.start()   
+    sleep(exit_time)
 
-# talk_to_godot()
+run_program()
